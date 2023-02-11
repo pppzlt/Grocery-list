8958a340928b8fe4e820266f70bd6ccd9e0ec9ea
Lantao home PC <lantaook@gmail.com>
2023-02-11 17:20:19 -0500

    save

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,14 @@
 const sequelize = require('../config/connection');
-const { User, Items } = require('../models');
+const { User, Items, Group } = require('../models');
 
 const itemsData = require('./items-seed.json');
 const userData = require('./user-seed.json');
 const groupData = require('./group-seed.json')
 
 const seedDatabase = async () => {
+  await sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
   await sequelize.sync({ force: true });
+  await sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
 
   for (const item of userData) {
     await User.create({...item});
