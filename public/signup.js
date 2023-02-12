d2d5e8b60b5f8fe63649f1981a0b7355a2841473
Lantao home PC <lantaook@gmail.com>
2023-02-12 09:15:17 -0500

    added signupjs and modified some hdb

---
+++ public/signup.js
@@ -0,0 +1,7 @@
+const input_email = document.querySelector('#inputEmail');
+const input_first_name = document.querySelector('#inputFirst');
+const input_last_name = document.querySelector('#inputLast');
+const input_tel = document.querySelector('#inputTel');
+const input_pwd = document.querySelector('#inputPwd');
+const input_pwd_check = document.querySelector('#inputPwdCheck');
+
