a4d4db43cdef7d47158749a4c15de08498a04618
Lantao home PC <lantaook@gmail.com>
2023-02-13 14:46:36 -0500

    drop /:id and combine

--- controllers/innerlist.js
+++ controllers/innerlist.js
@@ -3,20 +3,7 @@ const { User, List, Items } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("groceries");
-    // res.status(200).json({message: 'Hello there innerlist!'});
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/:id", async (req, res) => {
-  try {
-    let dbitems = await Items.findAll({
-      where: {
-        list_id: req.params.id,
-      },
-    });
+    let dbitems = await Items.findAll();
     console.log(dbitems);
     let items = dbitems.map((item) => item.get({ plain: true }));
     console.log(items);
