22b00059bd1bd6307256cc7dfdbf82f7641e31a9
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-13 11:41:45 -0500

    added function to populate blank card for new list

--- public/listPage.js
+++ public/listPage.js
@@ -1,7 +1,20 @@
+const addNew = document.getElementById("add-new");
+
+
+// FUNCTION TO POPULATE NEW LIST
+// CLICK EVENT LISTENER, IF Button is clicked, THEN new list is populated
+addNew.addEventListener('click', (event) => {
+    event.preventDefault();
+    let card = document.getElementById("newCard");
+    card.style.display = "block";
+    document.getElementById("newCardTitle").innerHTML = "Weekly List";
+    document.getElementById("newCardDesc").innerHTML = "This is your new list";
+})
 
 
 // EVENT LISTENERS FOR NAV MENU
 
-// ADD EDIT BUTTON TO BOTH CARDS
-
 // MAKE THE EDIT BUTTONS RESPONSIVE
+
+// EVENT LISTENER FOR DELETE ITEM BUTTON
+
